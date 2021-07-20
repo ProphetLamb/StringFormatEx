9895c2b1c2a6581ba38fd1bb0070384074e43c11
ProphetLamb <rene.carannante@outlook.de>
2021-07-20 10:16:28 +0200

    Move formatting arguments & -comparer to own file

---
+++ src/FormattingArgument.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringFormatEx
+{
+
+    public readonly struct FormattingArgument
+    {
+        public FormattingArgument(string symbol, object? value)
+        {
+            Symbol = symbol;
+            Value = value;
+        }
+
+        public readonly string Symbol;
+
+        public readonly object? Value;
+
+        public void Deconstruct(out string symbol, out object? value)
+        {
+            symbol = Symbol;
+            value = Value;
+        }
+    }
+
+    public sealed class FormattingArgumentSymbolComparer : IComparer<FormattingArgument>
+    {
+        public static FormattingArgumentSymbolComparer Default { get; } = new();
+
+        public int Compare(FormattingArgument x, FormattingArgument y)
+        {
+            return String.Compare(x.Symbol, y.Symbol, StringComparison.Ordinal);
+        }
+    }
+}
